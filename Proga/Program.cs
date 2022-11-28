9b8bd1bb969988f5334708824b300b728e798860
Your Name <you@example.com>
2022-11-28 19:53:05 +0300

    Proga

---
+++ Proga/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] word = { "Tom", "Samd", "Bobd", "hdk", "shdjd", "l" };
+for (int i = 0; i < 6; i++)
+{
+    if (word[i].Length < 4)
+    {
+        Console.WriteLine(word[i]);
+    }   
+}
